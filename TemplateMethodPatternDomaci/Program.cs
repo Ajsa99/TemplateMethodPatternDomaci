bc9b43914ed05e39f4b08509f3d97c25975ad88d
Ajsa Alibasic <ajsa.alibasic11@gmail.com>
2023-05-26 14:41:35 +0200

    first commit

---
+++ TemplateMethodPatternDomaci/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TemplateMethodPatternDomaci
+{
+    abstract class ZahtjevZaLicnuKartu
+    {
+        public void PosaljiZahtjev()
+        {
+            KorisnikSaljeZahtev();
+            ProveraInformacija();
+            OdobrenjeOdStraneSluzbenika();
+        }
+
+        protected abstract void KorisnikSaljeZahtev();
+        protected abstract void ProveraInformacija();
+        protected abstract void OdobrenjeOdStraneSluzbenika();
+
+    }
+
+    class ZakazivanjeTermina : ZahtjevZaLicnuKartu
+    {
+        protected override void KorisnikSaljeZahtev()
+        {
+            Console.WriteLine("Korisnik salje zahtev za termin za licnu kartu.");
+        }
+        protected override void ProveraInformacija()
+        {
+            Console.WriteLine("Proveravaju se informacije o Korisniku.");
+        }
+        protected override void OdobrenjeOdStraneSluzbenika()
+        {
+            Console.WriteLine("Sluzbenik odobrava zahtev i zakazuje termin.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZahtjevZaLicnuKartu zahtjev = new ZakazivanjeTermina();
+            zahtjev.PosaljiZahtjev();
+        }
+    }
+}
